00368cb382d86275d41e94b137a5812a91c2cba2
Bruno Di Giorgi <bruno@brunodigiorgi.it>
2016-02-23 17:42:37 +0100

    refactoring Wire

--- pyAudioGraph/Nodes/TimeDomainFeatures.py
+++ pyAudioGraph/Nodes/TimeDomainFeatures.py
@@ -1,19 +1,20 @@
 import numpy as np
 from ..AudioGraph import Node
-from ..Wire import Wire
+from ..Wire import InWire, AudioOutWire
 
 
 class RmsNode(Node):
     """Compute the Root Mean Square value of the current buffer"""
     def __init__(self, world):
         super().__init__(world)
-        self.w_in = Wire(self, Wire.audioRate, Wire.wiretype_input, world.buf_len)
-        self.w_out = Wire(self, Wire.controlRate, Wire.wiretype_output, world.buf_len)
+        self.w_in = InWire(self)
+        self.w_out = AudioOutWire(self, world.buf_len)
 
         self.in_wires.append(self.w_in)
         self.out_wires.append(self.w_out)
 
     def calc_func(self):
         buf_len = self.world.buf_len
-        s = np.sum(np.power(self.w_in._buf, 2))
+        in_array = self.w_in.buf
+        s = np.sum(np.power(in_array, 2))
         self.w_out.set_value(np.sqrt(s / buf_len))
--- pyAudioGraph/__init__.py
+++ pyAudioGraph/__init__.py
@@ -1,8 +1,3 @@
-from .AudioBuffer import AudioBuffer, RingBuffer, SortedBuffer, MovingPercentile
-from .AudioGraph import Node, Group
-from .Wire import Wire
 from .World import World
 from .AudioStream import AudioStreamWaveFile
-from .CmdQueue import LambdaCommand, AsyncCmdQueue
-from .Range import Range, RangeQueue
 from . import Nodes
