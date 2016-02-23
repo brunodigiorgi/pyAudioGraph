7b8bfabd92483124fc35f924e4b0d22cbc613547
Bruno Di Giorgi <bruno@brunodigiorgi.it>
2016-02-23 17:51:33 +0100

    refactoring Wire

--- pyAudioGraph/__init__.py
+++ pyAudioGraph/__init__.py
@@ -1,3 +1,6 @@
+from .AudioGraph import Node, Group	
 from .World import World
 from .AudioStream import AudioStreamWaveFile
+from .CmdQueue import LambdaCommand, AsyncCmdQueue		
+from .Range import Range, RangeQueue		
 from . import Nodes
