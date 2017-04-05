4e2a2ce0b575c6909778f2630546a1ccf8c3a6b6
Bruno Di Giorgi <bruno@brunodigiorgi.it>
2017-04-05 20:52:21 +0200

    new structure

--- pyAudioGraph/__init__.py
+++ pyAudioGraph/__init__.py
@@ -1,8 +1,9 @@
+from .AudioBuffer import AudioBuffer, RingBuffer
+from .Wire import InWire, OutWire, AudioOutWire
 from .AudioGraph import Node, Group
 from .World import World
 from .AudioStream import AudioStreamWaveFile
 from .CmdQueue import LambdaCommand, AsyncCmdQueue
 from .Range import Range, RangeQueue
-from . import PitchTrackerContext
-from . import PitchTrackerYin
+from .MovingViterbi import MovingViterbi
 from . import Nodes
--- setup.py
+++ setup.py
@@ -2,25 +2,25 @@ from setuptools import find_packages
 from setuptools import setup
 
 install_requires = [
-    'numpy',
-    ]
+  'numpy',
+]
 
 tests_require = [
-    'pytest',
-    ]
+  'pytest',
+]
 
-setup(name='PyAudioGraph',
-    version='0.1',
-    description='Python Audio Graph',
-    author='Bruno Di Giorgi',
-    author_email='massimiliano.zanoni@polimi.it',
-    url='https://github.com/brunodigiorgi/pyAudioGraph',
-    license="GPLv2",
-    packages=find_packages(),
-    include_package_data=False,
-    zip_safe=False,
-    install_requires=install_requires,
-    extras_require={
-       	'testing': tests_require,
-       	},
-    )
+setup(name='pyAudioGraph',
+      version='0.1',
+      description='Python Audio Graph',
+      author='Bruno Di Giorgi',
+      author_email='bruno@brunodigiorgi.it',
+      url='https://github.com/brunodigiorgi/pyAudioGraph',
+      license="GPLv2",
+      packages=find_packages(),
+      include_package_data=False,
+      zip_safe=False,
+      install_requires=install_requires,
+      extras_require={
+        'testing': tests_require,
+      },
+      )
