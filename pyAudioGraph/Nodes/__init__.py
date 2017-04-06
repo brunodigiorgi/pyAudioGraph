c6a20871c17e25d769fb99ce24fd891a7600c3af
Bruno Di Giorgi <bruno@brunodigiorgi.it>
2017-04-06 09:21:15 +0200

    fix init

--- pyAudioGraph/Nodes/__init__.py
+++ pyAudioGraph/Nodes/__init__.py
@@ -1,8 +1,8 @@
 from .IONodes import OutNode, InNode
 from .MixerNode import MixerNode, MonizerNode
 from .SamplerNode import SamplerNode
-from .GenNodes import LevelSlopeGen, SinOsc, SawOsc
-from .OpNode import OpMult
+from .GenNodes import AudioSlopeGen, ControlSlopeGen, SinOsc, SawOsc
+from .OpNode import AudioOpBinary, ControlOpBinary, AudioOpMult, ControlOpMult
 from .TimeDomainFeatures import RmsNode
 from .Recorder import ControlRateRecorder, AudioRateRecorder
 from .DiskInNode import DiskInNode
